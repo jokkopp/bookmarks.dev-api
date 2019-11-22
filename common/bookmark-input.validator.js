8e66286bd08a91b53b568f6e320fe348d74d7c8d
Adrian Matei <adrian.matei@interdiscount.ch>
2019-11-22 22:34:17 +0100

    move bookmark validation function to own file

---
+++ common/bookmark-input.validator.js
@@ -0,0 +1,53 @@
+const constants = require('./constants');
+const ValidationError = require('../models/validation.error');
+
+let validateBookmarkInput = function(request, response, bookmark) {
+
+  let validationErrorMessages = [];
+  if (bookmark.userId !== request.params.userId) {
+    validationErrorMessages.push("The userId of the bookmark does not match the userId parameter");
+  }
+  if(!bookmark.name) {
+    validationErrorMessages.push('Missing required attribute - name');
+  }
+  if(!bookmark.location) {
+    validationErrorMessages.push('Missing required attribute - location');
+  }
+  if(!bookmark.tags || bookmark.tags.length === 0) {
+    validationErrorMessages.push('Missing required attribute - tags');
+  }
+
+  if (bookmark.tags.length > constants.MAX_NUMBER_OF_TAGS) {
+    validationErrorMessages.push('Too many tags have been submitted - max allowed 8');
+  }
+
+  let blockedTags = '';
+  for (let i = 0; i < bookmark.tags.length; i++) {
+    const tag = bookmark.tags[i];
+    if (tag.startsWith('awesome')) {
+      blockedTags = blockedTags.concat(' ' + tag);
+    }
+  }
+
+  if (blockedTags) {
+    validationErrorMessages.push('The following tags are blocked:' + blockedTags);
+  }
+
+  if (bookmark.description) {
+    const descriptionIsTooLong = bookmark.description.length > constants.MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION;
+    if (descriptionIsTooLong) {
+      validationErrorMessages.push('The description is too long. Only ' + constants.MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION + ' allowed');
+    }
+
+    const descriptionHasTooManyLines = bookmark.description.split('\n').length > constants.MAX_NUMBER_OF_LINES_FOR_DESCRIPTION;
+    if (descriptionHasTooManyLines) {
+      validationErrorMessages.push('The description hast too many lines. Only ' + constants.MAX_NUMBER_OF_LINES_FOR_DESCRIPTION + ' allowed');
+    }
+  }
+
+  if(validationErrorMessages.length > 0){
+    throw new ValidationError('The bookmark you submitted is not valid', validationErrorMessages);
+  }
+}
+
+module.exports.validateBookmarkInput = validateBookmarkInput;
--- routes/users/personal-bookmarks.js
+++ routes/users/personal-bookmarks.js
@@ -17,6 +17,7 @@ const common = require('../../common/config');
 const config = common.config();
 
 const constants = require('../../common/constants');
+const BookmarkInputValidator = require('../../common/bookmark-input.validator');
 
 const HttpStatus = require('http-status-codes');
 
@@ -28,54 +29,7 @@ const showdown = require('showdown'),
 const keycloak = new Keycloak({scope: 'openid'}, config.keycloak);
 personalBookmarksRouter.use(keycloak.middleware());
 
-let validateBookmarkInput = function(request, response, bookmark) {
 
-  let validationErrorMessages = [];
-  if (bookmark.userId !== request.params.userId) {
-    validationErrorMessages.push("The userId of the bookmark does not match the userId parameter");
-  }
-  if(!bookmark.name) {
-    validationErrorMessages.push('Missing required attribute - name');
-  }
-  if(!bookmark.location) {
-    validationErrorMessages.push('Missing required attribute - location');
-  }
-  if(!bookmark.tags || bookmark.tags.length === 0) {
-    validationErrorMessages.push('Missing required attribute - tags');
-  }
-
-  if (bookmark.tags.length > constants.MAX_NUMBER_OF_TAGS) {
-    validationErrorMessages.push('Too many tags have been submitted - max allowed 8');
-  }
-
-  let blockedTags = '';
-  for (let i = 0; i < bookmark.tags.length; i++) {
-    const tag = bookmark.tags[i];
-    if (tag.startsWith('awesome')) {
-      blockedTags = blockedTags.concat(' ' + tag);
-    }
-  }
-
-  if (blockedTags) {
-    validationErrorMessages.push('The following tags are blocked:' + blockedTags);
-  }
-
-  if (bookmark.description) {
-    const descriptionIsTooLong = bookmark.description.length > constants.MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION;
-    if (descriptionIsTooLong) {
-      validationErrorMessages.push('The description is too long. Only ' + constants.MAX_NUMBER_OF_CHARS_FOR_DESCRIPTION + ' allowed');
-    }
-
-    const descriptionHasTooManyLines = bookmark.description.split('\n').length > constants.MAX_NUMBER_OF_LINES_FOR_DESCRIPTION;
-    if (descriptionHasTooManyLines) {
-      validationErrorMessages.push('The description hast too many lines. Only ' + constants.MAX_NUMBER_OF_LINES_FOR_DESCRIPTION + ' allowed');
-    }
-  }
-
-  if(validationErrorMessages.length > 0){
-      throw new ValidationError('The bookmark you submitted is not valid', validationErrorMessages);
-  }
-}
 
 /**
  * CREATE bookmark for user
@@ -86,7 +40,7 @@ personalBookmarksRouter.post('/', keycloak.protect(), AsyncWrapper.wrapAsync(asy
 
   const bookmark = bookmarkHelper.buildBookmarkFromRequest(request);
 
-  validateBookmarkInput(request, response, bookmark);
+  BookmarkInputValidator.validateBookmarkInput(request, response, bookmark);
 
   if (bookmark.shared) {
     const existingBookmark = await Bookmark.findOne({
@@ -218,7 +172,7 @@ personalBookmarksRouter.put('/:bookmarkId', keycloak.protect(), AsyncWrapper.wra
 
   const bookmark = bookmarkHelper.buildBookmarkFromRequest(request);
 
-  validateBookmarkInput(request, response, bookmark);
+  BookmarkInputValidator.validateBookmarkInput(request, response, bookmark);
 
   if (request.body.shared) {
     const existingBookmark = await Bookmark.findOne({
